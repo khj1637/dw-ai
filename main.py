16034c839b870a4a6dea787808c8d8e3261a48e6
khj1637 <37471933+khj1637@users.noreply.github.com>
2025-06-18 12:34:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ layout.load_css()
 layout.header()
 
 # URL 파라미터에서 page 값 가져오기
-query_params = st.experimental_get_query_params()
+query_params = st.query_params
 page = query_params.get("page", ["home"])[0]
 
 # 라우팅
